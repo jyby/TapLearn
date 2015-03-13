7628a31bb0d34c558290e0c038da785280fe3127
Jeremy 'LeJyBy' Barbay <jeremy@barbay.cl>
2015-03-12 23:42:11 -0300

    Explained why the development of TapLearn gets terminated.

--- taplearn.py
+++ taplearn.py
@@ -1,5 +1,5 @@
 """
-TapLearn v2.0
+TapLearn
 
 By the end of the first phase of its development, <<<TapLearn>>> should be a generic example of pedagogical application with a touch interface, to be used to study the effect of various features on both persistence and learning. In its most basic version, the application downloads a sequence of predicates from a server, present them to the learner in a random order, each to be validated or refuted by the learner, gives feedback to the user and submits the user's answers to a central server for later studies. The features envisioned include the addition of animations, colors, sounds, spaced memorization techniques. The studies envisioned include the effect of those features on <<<persistence>>> (how frequently and how long the learner uses the application) and <<<learning>>> (how fast the learner progresses in his/her learning).
 
